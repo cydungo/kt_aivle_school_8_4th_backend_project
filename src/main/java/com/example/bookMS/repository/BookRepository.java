4e6462449f7a65009c9da17697f08e6c871b56b0
nsg716 <1029cjswo@naver.com>
2025-12-05 10:06:46 +0900

    repository commit

--- src/main/java/com/example/bookMS/repository/BookRepository.java
+++ src/main/java/com/example/bookMS/repository/BookRepository.java
@@ -4,9 +4,4 @@ import com.example.bookMS.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    //findAll
-    //findById
-    //save
-    //deleteById
-
 }
